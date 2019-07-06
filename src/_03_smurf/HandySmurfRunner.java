bb06ca58a5828892e82121d5242bc90223d1dbb9
League Student <league@iMac-18.attlocal.net>
2019-07-06 11:32:28 -0700

    Module 2

---
+++ src/_03_smurf/HandySmurfRunner.java
@@ -0,0 +1,22 @@
+package _03_smurf;
+
+public class HandySmurfRunner {
+public static void main(String[] args) {
+Smurf HandySmurf = new Smurf("handySmurf");
+HandySmurf.eat();
+HandySmurf.getName();
+
+Smurf PapaSmurf = new Smurf("PapaSmurf");
+PapaSmurf.getName();
+PapaSmurf.getHatColor();
+PapaSmurf.isGirlOrBoy();
+
+Smurf Smurfette = new Smurf("Smurfette");
+Smurfette.getName();
+Smurfette.getHatColor();
+Smurfette.isGirlOrBoy();
+
+
+
+}
+}
